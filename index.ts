33cd95f8d6ab0facd3b362eac9a04e8f144bbe3f
Travis Wagner <trvswgnr@gmail.com>
2025-02-11 01:11:39 -0500

    wip

--- index.ts
+++ index.ts
@@ -14,52 +14,56 @@ if (!apiKey) {
 	process.exit(1);
 }
 
-const prompt = process.argv.slice(2).join(" ").trim();
+async function main() {
+	const prompt = process.argv.slice(2).join(" ").trim();
 
-if (!prompt) {
-	console.error(`prompt is required\n${USAGE}`);
-	process.exit(1);
-}
-
-if (prompt.startsWith("-h")) {
-	console.log(USAGE);
-	process.exit(0);
-}
-
-const response = await fetch(ENDPOINT_URL, {
-	method: "POST",
-	headers: {
-		"Content-Type": "application/json",
-	},
-	body: JSON.stringify({ prompt, apiKey }),
-});
-
-if (!response.ok) {
-	console.error("Failed to fetch from", ENDPOINT_URL);
-	console.error(response.status, response.statusText);
-	try {
-		const body = await response.json();
-		console.error(body);
-	} catch (error) {
-		console.error(error);
-	}
-	process.exit(1);
-}
-
-if (!response.body) {
-	console.error("No body");
-	process.exit(1);
-}
-
-const reader = response.body.getReader();
-
-while (true) {
-	const { done, value } = await reader.read();
-	if (done) {
-		process.stdout.write("\n");
-		break;
+	if (!prompt) {
+		console.error(`prompt is required\n${USAGE}`);
+		process.exit(1);
 	}
 
-	const chunk = decoder.decode(value, { stream: true });
-	process.stdout.write(chunk);
+	if (prompt.startsWith("-h")) {
+		console.log(USAGE);
+		process.exit(0);
+	}
+
+	const response = await fetch(ENDPOINT_URL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ prompt, apiKey }),
+	});
+
+	if (!response.ok) {
+		console.error("Failed to fetch from", ENDPOINT_URL);
+		console.error(response.status, response.statusText);
+		try {
+			const body = await response.json();
+			console.error(body);
+		} catch (error) {
+			console.error(error);
+		}
+		process.exit(1);
+	}
+
+	if (!response.body) {
+		console.error("No body");
+		process.exit(1);
+	}
+
+	const reader = response.body.getReader();
+
+	while (true) {
+		const { done, value } = await reader.read();
+		if (done) {
+			process.stdout.write("\n");
+			break;
+		}
+
+		const chunk = decoder.decode(value, { stream: true });
+		process.stdout.write(chunk);
+	}
 }
+
+main();
